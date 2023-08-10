4d8803817f997b02370136895dd68f4ed095b06e
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-10 12:21:59 +0300

    Implement Book has-many Rentals rln

--- book.rb
+++ book.rb
@@ -2,7 +2,13 @@ class Book
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
   end
 
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self
+  end
 end
