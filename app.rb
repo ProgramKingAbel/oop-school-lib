74ea6d62f775dd51879495214fed849b600c02c7
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-11 12:10:54 +0300

    Implement create person

--- app.rb
+++ app.rb
@@ -27,4 +27,22 @@ class App
       puts "#{index + 1}. [#{person.class}], id: #{person.id}, Name: #{person.name}, Age: #{person.age}"
     end
   end
+
+  def create_person
+    loop do
+      puts 'Do you want to create a student (1) or a teacher (2)? [Input the number:'
+      input = gets.chomp
+
+      case input
+      when '1'
+        create_student
+        break
+      when '2'
+        create_teacher
+        break
+      else
+        puts 'Invalid input.Please Choose 1 for student 2 for teacher.'
+      end
+    end
+  end
 end
