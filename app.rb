f3c1dc5e11390b199c96fa7099e3750a0b0940ff
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-11 11:58:18 +0300

    Implement List all people

--- app.rb
+++ app.rb
@@ -15,7 +15,16 @@ class App
   def list_all_books
     return puts '0 Books currently available' if @books.empty?
 
-    book_info = @books.each_with_index do |book, index| 
+    @books.each_with_index do |book, index|
       puts "#{index + 1}. Title: #{book.title} by Author: #{book.author}"
+    end
+  end
+
+  def list_all_people
+    return puts 'No people Registered' if @people.empty?
+
+    @people.each_with_index do |person, index|
+      puts "#{index + 1}. [#{person.class}], id: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+    end
   end
 end
